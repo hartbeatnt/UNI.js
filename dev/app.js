b82e7f733c9452cf9678298524c14a8676ad11bc
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-05-14 12:27:25 -0700

    scene container primed

--- dev/app.js
+++ dev/app.js
@@ -1,3 +1,12 @@
 const THREE = require('three');
+const CONTAINER = document.querySelector('#container')
+let CONTAINER_W = container.offsetWidth;
+let CONTAINER_H = container.offsetHeight;
+window.addEventListener('resize',()=>{
+  CONTAINER_W = container.offsetWidth;
+  CONTAINER_H = container.offsetHeight;
+})
 
-console.log("ayyy!")
+const particleScene = {
+
+}
