627064e98a88615addc446566a1dd071f8d7844f
nate hart <hartbeatnt@gmail.com>
2018-03-06 21:14:51 -0800

    added loop but haven't actually used it yet

---
+++ dev/UNI/UNILoop.js
@@ -0,0 +1,61 @@
+export default (callbacks={}, options={}) => {
+  const fn = () => {};
+  const fps = options.fps || 30;
+  const step = 1000 / fps;
+  const maxUpdates = options.maxUpdates || 240;
+  const resetOnPanic = options.resetOnPanic || true;
+
+  const onBegin = callbacks.onBegin || fn;
+  const onComplete = callbacks.onComplete || fn;
+  const update = callbacks.update || fn;
+  const draw = callbacks.draw || fn;
+  const panic = callbacks.panic || fn;
+  
+  let animationFrame = null;
+  let running = false;
+  let prevTime = 0;
+  let deltaTime = 0;
+
+  let loop = (time=performance.now()) => {
+    animationFrame = requestAnimationFrame(loop);
+
+    let updateSteps = 0;
+    deltaTime += time - prevTime;
+    prevTime = time;
+    running = true;
+
+    onBegin(time, deltaTime);
+    
+    while(deltaTime >= step) {
+      update(step);
+      deltaTime -= step;
+
+      if(++updateSteps >= maxUpdates) {
+        panic();
+        if(resetOnPanic) deltaTime = 0;
+        break;
+      }
+    }
+    
+    draw(deltaTime / time);
+    
+    return onComplete(animationFrame);
+  };
+
+  loop.stop = () => {
+    cancelAnimationFrame(animationFrame);
+    running = false;
+  };
+
+  loop.run = () => {
+    running = true;
+    prevTime = performance.now();
+    animationFrame = requestAnimationFrame(loop);
+  }
+
+  loop.isRunning = () => {
+    return running;
+  }
+
+  return loop;
+}
---
+++ dev/uni.example.js
@@ -0,0 +1,74 @@
+import Keyboard from './UNI/systems/Keyboard';
+
+import UNIVerse from './UNI/UNIVerse';
+import UNIPhysVerse from './UNI/UNIPhysVerse';
+import UNIPhysCube from './UNI/UNIPhysCube';
+import UNICube from './UNI/UNICube';
+import UNIPartSys from './UNI/UNIPartSys';
+import UNILight from './UNI/UNILight';
+import UNILoop from './UNI/UNILoop';
+
+export default () => {
+
+  const THREE = require('three');
+  const CANNON = require('cannon');
+
+  const container = document.querySelector('#container')
+  const sceneSettings = { 
+    clearColor: "#24f46a",
+  } 
+
+  const universe = new UNIVerse(container, sceneSettings);
+
+  const partSys = new UNIPartSys();
+  const cube1 = new UNICube({
+    position:{x:-1},
+    material: new THREE.MeshLambertMaterial({ color:0x2194ce }),
+  });
+  const cube2 = new UNICube({
+    position:{x:1},
+    material: new THREE.MeshBasicMaterial({ color:0x2194ce }),
+  });
+
+  const floor = new UNICube({
+    position: { y: -4, z: -2.5 },
+    size: 5,
+    material: new THREE.MeshLambertMaterial({ color:0x2194ce }),
+  })
+
+  const ambLight = new UNILight()
+
+  const ptLight = new UNILight({
+    type:'Point',
+    position: { x: 0, y: 0, z: -0.25 },
+    intensity: 2,
+  })
+
+
+
+  //  TODO: allow addEntity to add multiple entities in a row
+  universe.addEntity(partSys);
+  universe.addEntity(ambLight);
+  universe.addEntity(ptLight);
+  universe.addEntity(cube1);
+  universe.addEntity(cube2);
+  universe.addEntity(floor);
+
+
+  const keyboard = new Keyboard('ArrowUp','ArrowDown')
+  const animate = time => {
+      requestAnimationFrame(animate);
+      keyboard.ArrowUp && moveLightZ(ptLight, 1);
+      keyboard.ArrowDown && moveLightZ(ptLight, -1);
+      universe.render(deltaTime);
+  }
+
+  const moveLightZ = (light,amt) => {
+    light.mesh.position.z+=.01*amt
+  }
+
+  let oldTime = performance.now();
+  let deltaTime = performance.now();
+  animate(oldTime)
+
+}
